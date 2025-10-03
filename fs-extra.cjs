40b3f4e839161f65a2df725975314a21f1d473d7
quangtk2005 <quangtk205@gmail.com>
2025-10-03 22:34:26 +0700

    Add lder Media

--- fs-extra.cjs
+++ fs-extra.cjs
@@ -1,8 +1,8 @@
 const fs = require('fs-extra');
 // Async with promises:
-// fs.copy('public', 'dist/public')
-//   .then(() => console.log('public success!'))
-//   .catch(err => console.error(err));
+fs.copy('media', 'dist/media')
+  .then(() => console.log('media success!'))
+  .catch(err => console.error(err));
 
 // fs.copy('views', 'dist/views')
 //   .then(() => console.log('views success!'))
